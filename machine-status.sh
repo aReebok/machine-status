7c7623e59b9a50f84cabae52f065da9ea41a62de
Areeba Khan <kahnareeba@gmail.com>
2021-11-18 17:50:08 -0600

    Tries to fix issue with printing out new lines (needs to be checked again)

--- machine-status.sh
+++ machine-status.sh
@@ -89,9 +89,9 @@ function log_status () {
     i=${curr_date}
     while [[ ${i} != ${a_week_ago} ]]
     do
-        temp_date=$( date --date="${i}" +%c | awk  '{print $3, $2}')
-        echo ${errors} | grep "${temp_date}" # | awk '{print $0, "\n"}'  # prints new line after each grep.
-        echo "" 
+        temp_date=$( date --date="${i}" +%c | awk  '{print "\n"}END{print $3}')
+        echo ${errors} | grep "${temp_date}"   # | awk '{print $0, "\n"}'  # prints new line after each grep.
+
         i=$( date -d "${i} - 1 day" +%F )
     done
 
