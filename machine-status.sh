e780af3b388d838ae18e20cd5d4be9415899aefd
Areeba Khan <kahnareeba@gmail.com>
2021-11-18 19:49:20 -0600

    Fixes compile issues with log_status

--- machine-status.sh
+++ machine-status.sh
@@ -89,8 +89,8 @@ function log_status () {
     i=${curr_date}
     while [[ ${i} != ${a_week_ago} ]]
     do
-        temp_date=$( date --date="${i}" +%c | awk  '{print "\n"}END{print $3}')
-        echo ${errors} | grep "${temp_date}"   # | awk '{print $0, "\n"}'  # prints new line after each grep.
+        temp_date=$( date --date="${i}" +%c | awk  '{print $3}')
+        echo ${errors} | grep "${temp_date}"  | awk 'BEGIN{FS="${temp_date};OFS=\"\n=> ${temp_date}"}'  # | awk '{print $0, "\n"}'  # prints new line after each grep.
 
         i=$( date -d "${i} - 1 day" +%F )
     done
