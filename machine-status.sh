74612d4680173f3d251e1dad2a4e0bb909e7407f
Areeba Khan <kahnareeba@gmail.com>
2021-11-24 16:51:36 -0600

    Fixes issue with root storage displaying

--- machine-status.sh
+++ machine-status.sh
@@ -4,7 +4,7 @@
 #  print out date and time -----
 
 function print_date () {
-    echo date
+    echo ${date}
     echo
 }
 
@@ -13,26 +13,21 @@ function preamble () {
     echo -n "Hostname: ${HOSTNAME} @ ip address: "
     ip ad | grep 127.0.0. | cut -d " " -f 6 | cut -d "/" -f 1 | head -n 1
     echo 
-    echo -n ">>"
     hostnamectl | grep "Operating System"
-
     echo
 }
 
 ### SERVICES 
     function ntp_status () {
         #  print out NTP status    -----
-        echo ----- $'\033[1;36m'Services $'\033[0m'----------------------------$
-        echo -n ">>   NTP: " 
+        echo SERVICES
+        echo -n ">>  " 
         timedatectl status | grep "NTP"
-
         if [ $? = 1 ]
         then
-            echo $'\033[0;31m'NTP not set up.$'\033[0m'
-        else
-                echo "NTP is set up"
+            echo "NTP not set up."
         fi
-
+        echo
 }        
     function fail2ban () {
         echo -n ">>  fail2ban: "
@@ -42,9 +37,9 @@ function preamble () {
         if test -f "$FILE"; then
             /etc/init.d/fail2ban status | grep "active (running)\|inactive" | cut -d " " -f 5
         else
-            echo $'\033[0;31m'fail2ban not set up.$'\033[0m'
+            echo "not set up."
         fi
-
+        echo 
     }
 
 function services () {
@@ -53,19 +48,19 @@ function services () {
 }
 
 function root_space() {
-    root_name=$(df -h | grep root | awk '{print $1}')
-    root_size=$(df -h | grep root | awk '{print $2}')
-    root_usage=$(df -h | grep root | awk '{print $5}')
-    echo 
-    echo ----- $'\033[1;36m'ROOT SPACE $'\033[0m'--------------
+    root_name=$(df -h | grep -f '/' | awk '{print $1}')
+    root_size=$(df -h | grep -f '/' | awk '{print $2}')
+    root_usage=$(df -h | grep -f '/' | awk '{print $5}')
+    echo ROOT SPACE
     echo "root (/) of size ${root_size} is ${root_usage} full."
+    echo
 }
 
 function disk_space () {
     # print out disk space and memory
-    echo
-    echo ----- $'\033[1;36m'DISKS OVER CAPACITY OF 80% $'\033[0m'-----------------------------------------
+    echo DISKS OVER CAPACITY OF 80%
     df -h | awk '0+$5 >= 80 {print $1, $2, $5, $6}' | column -t
+    echo
 }
 
 function mem_usage () {
